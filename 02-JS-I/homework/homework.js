eaed40f4da09b188218b2ea6cfbd2af442fdef23
marco <marcoabanda.89@gmail.com>
2022-02-25 13:16:51 -0600

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -224,13 +224,13 @@ function esPositivo(numero) {
     return "Es negativo";
   
 }
-esPositivo(5)
+esPositivo(5);
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
- return "Hello World" + str;
+ console.log("Hola mundo"+ str);
 }
 agregarSimboloExclamacion('!');
 
