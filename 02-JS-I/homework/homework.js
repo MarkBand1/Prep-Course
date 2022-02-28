59b1cb76dd8f0b1f84ab65760bb03ce3c51231a5
marco <marcoabanda.89@gmail.com>
2022-02-28 12:02:37 -0600

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -227,16 +227,16 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-return"Hola mundo"+ str;
+return str + '!';
 }
-agregarSimboloExclamacion('!');
+agregarSimboloExclamacion('Hello World');
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
- return nombre, apellido;
+  var combinado = nombre + ' ' + apellido;
+  return combinado;
 }
    combinarNombres('Marco Antonio ', 'Banda');
 
