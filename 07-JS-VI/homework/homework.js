611444d83774fb99d7ba96924aa71e044c0eac5f
marco <marcoabanda.89@gmail.com>
2022-03-29 15:30:26 -0600

    sixth commit

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,7 +4,9 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  return nombre[0].toUpperCase() + nombre.slice(1);
 }
+//mayuscula(mario);
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
